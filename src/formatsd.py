982c54cec2301267489a0ca96cf3dbaf10e3bbf6
fdsig <fdesigley@gmail.com>
2022-04-19 15:08:13 +0100

    while looping format.py

--- src/formatsd.py
+++ src/formatsd.py
@@ -1,46 +1,54 @@
 
-from subprocess import Popen,PIPE, check_output
+from subprocess import CalledProcessError, Popen,PIPE, check_output
 from pathlib import Path
 from sys import stdout
 from xml.etree.ElementTree import PI
-ps = Popen(('df'), stdout=PIPE)
-out = Popen(('grep','sd'), stdin=ps.stdout,stdout=PIPE,stderr=PIPE)
-#ps.wait()
-out_str, err = out.communicate()
-out_str = out_str.decode('utf8')
 
-while 'sd' in out_str:
+
+cont = 'y'
+
+while cont=='y':
     ps = Popen(('df'), stdout=PIPE)
     out = Popen(('grep','sd'), stdin=ps.stdout,stdout=PIPE,stderr=PIPE)
     #ps.wait()
     out_str, err = out.communicate()
     out_str = out_str.decode('utf8')
-    mounted = out_str.split('%')[-1].strip('\n').replace(' ', '')
     location = out_str.split(' ')[0]
     print(location)
 
-get, url = 'wget','https://cdimage.ubuntu.com/releases/20.04.4/release/ubuntu-20.04.4-preinstalled-server-arm64+raspi.img.xz'
+    while 'sd' in location:
+        mounted = out_str.split('%')[-1].strip('\n').replace(' ', '')
+        location = out_str.split(' ')[0]
+        try:
+            unmount = check_output(['umount',mounted])
+            ps = Popen(('df'), stdout=PIPE)
+            out = Popen(('grep','sd'), stdin=ps.stdout,stdout=PIPE,stderr=PIPE)
+            #ps.wait()
+            out_str, err = out.communicate()
+            out_str = out_str.decode('utf8')
+        except CalledProcessError:
+            print('nothing to unmount')
+            
+        print(location)
 
-if 'mounted' in globals():
-    unmount = check_output(['umount',mounted])
+    get, url = 'wget','https://cdimage.ubuntu.com/releases/20.04.4/release/ubuntu-20.04.4-preinstalled-server-arm64+raspi.img.xz'
 
-    print(unmount)
+    #3 cretating a disk image from compressed
+    if not Path(url.split('/')[-1]).exists:
+        get = check_output([get,url], stdin=ps.stdout)
 
-#3 cretating a disk image from compressed
-if not Path(url.split('/')[-1]).exists:
-    get = check_output([get,url], stdin=ps.stdout)
+    unpack = 'xzcat','ubuntu-20.04.4-preinstalled-server-arm64+raspi.img.xz'
 
-unpack = 'xzcat' , 'ubuntu-20.04.4-preinstalled-server-arm64+raspi.img.xz'
+    imager = 'dd', 'of=/dev/sda bs=32M status=progress'
 
-imager = 'dd', 'of=/dev/sda bs=32M status=progress'
+    img_un = Popen(unpack,stdout=PIPE,stderr=PIPE)
+    imager = Popen(imager,stdin=img_un.stdout, stdout=PIPE)
 
-img_un = Popen(unpack,stdout=PIPE,stderr=PIPE)
-imager = Popen(imager,stdin=img_un.stdout, stdout=PIPE)
+    print(imager.communicate())
 
-imager.communicate()
+    mnt = 'mount', '/dev/sda1', 'sd'
 
-
-#cont = input('do you want to continue formating?')
+    cont = input('do you want to continue formating? (y,n)')
 
 
 #command = f'sudo umount {mounted} && mkfs.vfat -F32 -v {location}'
